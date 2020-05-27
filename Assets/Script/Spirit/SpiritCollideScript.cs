eb2efbf91fb4dd9cd273a1c2e3df9d1c4044d3cf
double Z <doubleZ0108@163.com>
2020-05-27 17:28:11 +0800

    finish spirit

---
+++ Assets/Script/Spirit/SpiritCollideScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritCollideScript : MonoBehaviour
+{
+    private int CrashNum = 0;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name != "Plane" && collision.gameObject.name != "Wall")
+        {
+            CrashNum += 1;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnGUI()
+    {
+        Rect area = new Rect(10.0f, 10.0f, 300.0f, 40.0f);
+        GUILayout.BeginArea(area);
+
+        GUILayout.Label("Crashed: " + CrashNum.ToString());
+
+        GUILayout.EndArea();
+    }
+}
