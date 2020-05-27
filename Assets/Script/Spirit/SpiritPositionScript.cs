230779cc16879eb4d9a5e7514c5e3be0874debe1
double Z <doubleZ0108@163.com>
2020-05-27 17:28:11 +0800

    finish spirit

---
+++ Assets/Script/Spirit/SpiritPositionScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritPositionScript : MonoBehaviour
+{
+    CharacterController spirit;
+    public float gravity = 9.8f;
+
+    void Start()
+    {
+        spirit = GetComponent<CharacterController>();
+    }
+
+    void DownToGround()
+    {
+        if (!spirit.isGrounded)
+        {
+            Vector3 moveVec = new Vector3(0, 0, 0);
+            moveVec += Vector3.down * gravity * Time.deltaTime;
+        }
+    }
+
+    void Update()
+    {
+        DownToGround();
+    }
+}
